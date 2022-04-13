5b44793f1c14b48e9827ae7b433222c5b5dccfbf
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-04-13 13:15:22 +0200

    Add support of elixir 1.12
    It is the version provided by Debian testing/bookworm

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirPcscSample.MixProject do
     [
       app: :elixir_pcsc_sample,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
