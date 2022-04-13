b1f8d6126466eacacc9e3408325a945910155686
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-04-13 17:09:42 +0200

    Use IO.puts() for a better formatting

--- blog.exs
+++ blog.exs
@@ -1,9 +1,9 @@
-IO.puts "Listing card readers"
+# list card readers
 {:ok, readers} = :pcsc_card_db.list_readers()
 
 # use the fist reader
 [reader | _] = readers
-IO.inspect "Using reader: " <> reader
+IO.puts("Using reader: " <> reader)
 
 # connect to the card
 {:ok, card} = :pcsc_card.start_link(reader, :shared, [:t1, :t0])
