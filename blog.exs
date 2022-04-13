93d142c347126fc517632aa82a620b64c275f4c4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-04-13 13:33:10 +0200

    connect to the first reader

--- blog.exs
+++ blog.exs
@@ -1,3 +1,9 @@
 IO.puts "Listing card readers"
 {:ok, readers} = :pcsc_card_db.list_readers()
-IO.inspect readers
+
+# use the fist reader
+[reader | _] = readers
+IO.inspect "Using reader: " <> reader
+
+# connect to the card
+{:ok, card} = :pcsc_card.start_link(reader, :shared, [:t1, :t0])
