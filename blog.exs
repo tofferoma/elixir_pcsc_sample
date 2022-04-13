4cd92f800a5d329276e6444b3ca35e02bc525cbb
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-04-13 13:34:51 +0200

    Send select APDU
    Build fails with:
     mix run blog.exs
    
    13:33:59.635 [error] calling logger:remove_handler(default) failed: :error {:badmatch, {:error, {:not_found, :default}}}
    13:33:59.673 [info] Application lager started on node nonode@nohost
    13:33:59.679 [info] Application pcsc started on node nonode@nohost
    13:33:59.679 [info] Application elixir_pcsc_sample started on node nonode@nohost
    ** (TokenMissingError) blog.exs:14:1: missing terminator: ) (for "(" starting at line 13)
        (elixir 1.12.2) lib/code.ex:1261: Code.require_file/2
        (mix 1.12.2) lib/mix/tasks/run.ex:146: Mix.Tasks.Run.run/5
        (mix 1.12.2) lib/mix/tasks/run.ex:86: Mix.Tasks.Run.run/1
        (mix 1.12.2) lib/mix/task.ex:394: anonymous fn/3 in Mix.Task.run_task/3
        (mix 1.12.2) lib/mix/cli.ex:84: Mix.CLI.run_task/2

--- blog.exs
+++ blog.exs
@@ -7,3 +7,7 @@ IO.inspect "Using reader: " <> reader
 
 # connect to the card
 {:ok, card} = :pcsc_card.start_link(reader, :shared, [:t1, :t0])
+
+# send select APDU
+aid = << 160, 0, 0, 0, 98, 99, 1, 12, 6, 0 >>
+{:ok, replies} = :pcsc_card.command(card, #apdu_command{cla = :iso, ins = :select, p1 = 0, p2 = 10, data = aid})
